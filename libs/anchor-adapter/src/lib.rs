a5dcc814682f0e49907329f97a2bc462d5b5d1aa
Ondra Chaloupka <chalda@chainkeepers.io>
2022-12-08 12:19:22 +0100

    add missing discriminator implementation for marinade-anchor-sdk

--- libs/anchor-adapter/src/lib.rs
+++ libs/anchor-adapter/src/lib.rs
@@ -9,6 +9,7 @@ use micro_anchor::{AccountDeserialize, AccountDeserializeError, Discriminator, O
 pub struct Wrapper<T>(T);
 
 impl<T: Discriminator> AnchorDiscriminator for Wrapper<T> {
+    const DISCRIMINATOR: [u8; 8] = T::DISCRIMINATOR;
     fn discriminator() -> [u8; 8] {
         T::DISCRIMINATOR
     }
